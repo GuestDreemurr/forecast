6c44a53044cd020909f852eb435aee81b950a985
GuestDreemurr <55953204+GuestDreemurr@users.noreply.github.com>
2026-09-21 02:26:15 -0400

    finished config

--- configure.py
+++ configure.py
@@ -29,7 +29,7 @@ from tools.project import (
 # Game versions
 DEFAULT_VERSION = 0
 VERSIONS = [
-    "GAMEID",  # 0
+    "HAFE",  # Forecast Channel USA/NTSC
 ]
 
 parser = argparse.ArgumentParser()
